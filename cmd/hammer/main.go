b56a0968289ce4a88519045275bce4b8f1a0fdb0
Lenin Mehedy <lenin.mehedy@swirldslabs.com>
2025-08-23 17:23:44 +1000

    fix: scaffold hammer cli
    Signed-off-by: Lenin Mehedy <lenin.mehedy@swirldslabs.com>

---
+++ cmd/hammer/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// hammer account create --nodes "node1,node2" --mirror-node mirror:80 --bot 100 --duration 10s --tps 10 // Total TPS = 100 * 10 = 1000
+// hammer account create --config local.yml --bot 1000 --duration 10s --tps 100
+
+func main() {
+	fmt.Println("Not implemented yet")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/leninmehedy/solo-chaos
+
+go 1.21
